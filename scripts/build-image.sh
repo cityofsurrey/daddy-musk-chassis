6f9133dcbeec9c1f765715826c35ae80b65a89e9
Andrew Song <andrewgnos@gmail.com>
2017-07-27 16:50:31 -0700

    delete build-image script

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,17 +1,1 @@
 # #!/bin/bash
-#
-# set -eo pipefail
-#
-# source ./scripts/utils.sh
-#
-# echo "--- Building app"
-# yarn install
-# yarn run build
-#
-# VERSION=$(package_version)
-#
-# echo "--- Building docker image"
-# docker build \
-#   --pull \
-#   --tag cityofsurrey/polltal-app:${VERSION} \
-#   .
