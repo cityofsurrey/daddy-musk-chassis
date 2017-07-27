a6c05c7657021cbfd5ed19750e3f1507639b29cf
Andrew Song <andrewgnos@gmail.com>
2017-07-26 23:29:00 -0700

    build image script

---
+++ scripts/build-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source ./scripts/utils.sh
+
+echo "--- Building app"
+yarn install
+yarn run build
+
+VERSION=$(package_version)
+
+echo "--- Building docker image"
+docker build \
+  --pull \
+  --tag cityofsurrey/mysurrey-app:${VERSION} \
+  .
